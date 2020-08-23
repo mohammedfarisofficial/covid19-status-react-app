0a10a76d6f5a9588d58b03c283ad7e98c97443a8
mohammedfarisofficial <mohammedfarisofficial@gmail.com>
2020-08-23 14:04:46 +0530

    Updates

---
+++ precache-manifest.5db8dd3f5bc44e2e13bbd01cfae51ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9c30683ddfe43f24e3f8dc663e95f1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d3cedfcf76dcf216583a",
+    "url": "/my-app/static/css/main.0da8dc58.chunk.css"
+  },
+  {
+    "revision": "7654730149d6f18fe5cf",
+    "url": "/my-app/static/js/2.e134d693.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e134d693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cedfcf76dcf216583a",
+    "url": "/my-app/static/js/main.1f45d93e.chunk.js"
+  },
+  {
+    "revision": "f899f149cee7fc61157c",
+    "url": "/my-app/static/js/runtime-main.e8600da2.js"
+  }
+]);
